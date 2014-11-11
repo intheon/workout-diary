bfe5e69aadc8a6f152d1d8a5f2ce92d67ef9bca4
intheon <allobon@gmail.com>
2014-11-11 23:04:49 +0000

    and the db

--- wholegrain.sql
+++ wholegrain.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 24, 2014 at 05:57 PM
+-- Generation Time: Nov 12, 2014 at 12:01 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -35,18 +35,14 @@ CREATE TABLE IF NOT EXISTS `athlete` (
   `bmr` varchar(30) NOT NULL,
   `acn` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `athlete`
 --
 
 INSERT INTO `athlete` (`id`, `name`, `sex`, `weight`, `height`, `bmr`, `acn`) VALUES
-(1, 'test', 'test', 'test', 'test', 'test', 'test'),
-(2, 'root', 'Female', '78', '172', '1873', '2204'),
-(3, 'Ben', 'Male', '79', '172', '1800', '2222'),
-(4, 'blah', 'Male', 'dwedewd', 'wedwed', 'wedwedwe', 'dwed'),
-(5, 'Steve', 'Female', 'Man', 'Wow', 'Cheese', 'Hax');
+(3, 'Ben', 'Male', '79', '172', '1800', '2222');
 
 -- --------------------------------------------------------
 
@@ -56,16 +52,69 @@ INSERT INTO `athlete` (`id`, `name`, `sex`, `weight`, `height`, `bmr`, `acn`) VA
 
 CREATE TABLE IF NOT EXISTS `cardio_complete` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `session_id` int(10) NOT NULL,
-  `e1` varchar(30) NOT NULL,
-  `e2` varchar(30) NOT NULL,
-  `e3` varchar(15) NOT NULL,
-  `e4` varchar(15) NOT NULL,
-  `e5` varchar(15) NOT NULL,
-  `e6` varchar(15) NOT NULL,
-  `e7` varchar(15) NOT NULL,
+  `exercise_name` varchar(255) NOT NULL,
+  `date_done` varchar(255) NOT NULL,
+  `calories_total` int(10) NOT NULL,
+  `minutes_quantity` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+
+--
+-- Dumping data for table `cardio_complete`
+--
+
+INSERT INTO `cardio_complete` (`id`, `exercise_name`, `date_done`, `calories_total`, `minutes_quantity`) VALUES
+(17, 'Walking', 'Tuesday the 11th of November', 476, '34'),
+(18, 'Running', 'Tuesday the 11th of November', 1904, '34'),
+(19, 'Walking', 'Tuesday the 11th of November', 476, '34'),
+(20, 'Running', 'Tuesday the 11th of November', 168, '3'),
+(21, 'Dancing', 'Tuesday the 11th of November', 782, '23'),
+(22, 'Skateboarding', 'Tuesday the 11th of November', 0, '23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cardio_exercises`
+--
+
+CREATE TABLE IF NOT EXISTS `cardio_exercises` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `exercise_name` varchar(255) NOT NULL,
+  `calorie_consumption_per_minute` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `cardio_exercises`
+--
+
+INSERT INTO `cardio_exercises` (`id`, `exercise_name`, `calorie_consumption_per_minute`) VALUES
+(1, 'Walking', '14'),
+(2, 'Running', '56'),
+(7, 'Dancing', '23'),
+(8, 'Skateboarding', '14'),
+(9, 'Zombosis', '34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diet`
+--
+
+CREATE TABLE IF NOT EXISTS `diet` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `date_done` varchar(255) NOT NULL,
+  `json` varchar(255) NOT NULL,
+  `total_calories` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `diet`
+--
+
+INSERT INTO `diet` (`id`, `date_done`, `json`, `total_calories`) VALUES
+(1, 'Tuesday the 11th of November', '{"itemnumber1":{"huge burger":"800"},"itemnumber2":{"soup":"300"},"itemnumber3":{"chocolate":"179"}}', '1279');
 
 -- --------------------------------------------------------
 
