4a1ecb97012b49bf7706c434102d1c3152619ac0
intheon <allobon@gmail.com>
2014-12-08 17:16:55 +0000

    done for today
    done for today

--- wholegrain.sql
+++ wholegrain.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2014 at 05:43 PM
+-- Generation Time: Dec 08, 2014 at 06:16 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -39,14 +39,15 @@ CREATE TABLE IF NOT EXISTS `athlete` (
   `calories` double NOT NULL,
   `gym_visits_per_week` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
 --
 -- Dumping data for table `athlete`
 --
 
 INSERT INTO `athlete` (`id`, `username`, `name`, `email`, `gender`, `age`, `activity`, `weight`, `height`, `calories`, `gym_visits_per_week`) VALUES
-(8, 'intheon', 'Ben', 'allobon@gmail.com', 'male', 24, 'light', 79, '171', 2517.77075, 4);
+(20, 'Ashbury', 'Ben Smith', 'allobon@gmail.com', 'male', 24, 'light', 75, '171', 2133.0216, 4),
+(21, 'intheon', 'Ben', 'allobon@gmail.com', 'male', 24, 'light', 74, '171', 2116.9452, 4);
 
 -- --------------------------------------------------------
 
@@ -61,16 +62,15 @@ CREATE TABLE IF NOT EXISTS `auth` (
   `athlete_id` int(10) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
 
 --
 -- Dumping data for table `auth`
 --
 
 INSERT INTO `auth` (`id`, `username`, `password`, `athlete_id`) VALUES
-(8, 'intheon', '$2y$10$xfosXxFGWvSGtynFoMgeTO.Da6KWTiiIRfWsakJpwAmjsuwJOptCm', 8),
-(9, 'Gravy', '$2y$10$h.i.xNa7uJGtCMkGOZV6Q.Z5HWqGmfj/zqm7gDmSECRXj3RlqCcJe', 9),
-(10, 'ham', '$2y$10$rdVk/7rtKEO0pECaDqGYQuIsspeQLoUmJ9BsSJAut4RL9lwKxjHIO', 10);
+(22, 'Ashbury', '$2y$10$ihVc33fZdLzt8TxSh4exaO7KLIRdfGhBdo1YkPerXuF8RW.I7qv06', 20),
+(23, 'intheon', '$2y$10$55V836ntg6KY.pOAXA4Gb.8IRc7WF1eLG14njkei7G7Cg1XRgRZ3i', 21);
 
 -- --------------------------------------------------------
 
@@ -146,35 +146,39 @@ CREATE TABLE IF NOT EXISTS `diet` (
   `date_done` varchar(255) NOT NULL,
   `json` varchar(255) NOT NULL,
   `total_calories` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
 
 --
 -- Dumping data for table `diet`
 --
 
-INSERT INTO `diet` (`id`, `date_done`, `json`, `total_calories`) VALUES
-(1, 'Tuesday the 11th of November', '{"itemnumber1":{"huge burger":"800"},"itemnumber2":{"soup":"300"},"itemnumber3":{"chocolate":"179"}}', '1279'),
-(2, 'Monday the 17th of November', '{"itemnumberNaN":{"Apple":"50"}}', '50'),
-(6, 'Tuesday the 18th of November', '{"itemnumber1":{"Beef":"140"},"itemnumber2":{"Chicken":"200"}}', '340'),
-(7, 'Tuesday the 18th of November', '{"itemnumber1":{"Coffee":"324"}}', '324'),
-(8, 'Tuesday the 18th of November', '{"itemnumber1":{"Cheese":"783"}}', '783'),
-(9, 'Tuesday the 18th of November', '{"itemnumber1":{"Beer":"234"}}', '234'),
-(10, 'Tuesday the 18th of November', '{"itemnumber1":{"cheese":"687"},"itemnumber2":{"beef":"234"}}', '921'),
-(11, 'Wednesday the 19th of November', '{"itemnumber1":{"Bru":"187"}}', '187'),
-(12, 'Wednesday the 19th of November', '{"itemnumber1":{"Sand":"34"},"itemnumber2":{"Pigs":"674"}}', '708'),
-(13, 'Wednesday the 19th of November', '{"itemnumber1":{"Milk":"321"}}', '321'),
-(14, 'Thursday the 20th of November', '{"itemnumber1":{"Coffee":"234"}}', '234'),
-(15, 'Thursday the 20th of November', '{"itemnumber1":{"2 Mexican Veg Burgers":"456"}}', '456'),
-(16, 'Friday the 21st of November', '{"itemnumber1":{"food":"222"},"itemnumber2":{"cheese":"342"}}', '564'),
-(17, 'Monday the 24th of November', '{"itemnumber2":{"cake":"231"}}', '231'),
-(18, 'Thursday the 27th of November', '{"itemnumber1":{"big soup":"450"}}', '450'),
-(19, 'Friday the 5th of December 2014', '{"itemnumber1":{"crumpets":"378"},"itemnumber2":{"coffee":"230"}}', '608'),
-(20, 'Friday the 5th of December 2014', '{"itemnumber1":{"banana":"89"}}', '89'),
-(21, 'Friday the 5th of December 2014', '{"itemnumber1":{"More Coffee":"200"}}', '200'),
-(22, 'Friday the 5th of December 2014', '{"itemnumber1":{"Cheese Bread":"230"}}', '230'),
-(23, 'Friday the 5th of December 2014', '{"itemnumber1":{"Big Soup":"200"}}', '200'),
-(24, 'Friday the 5th of December 2014', '{"itemnumber1":{"2 Shortbread":"200"},"itemnumber2":{"Banana":"100"}}', '300');
+INSERT INTO `diet` (`id`, `date_done`, `json`, `total_calories`, `owner`) VALUES
+(1, 'Tuesday the 11th of November', '{"itemnumber1":{"huge burger":"800"},"itemnumber2":{"soup":"300"},"itemnumber3":{"chocolate":"179"}}', '1279', ''),
+(2, 'Monday the 17th of November', '{"itemnumberNaN":{"Apple":"50"}}', '50', ''),
+(6, 'Tuesday the 18th of November', '{"itemnumber1":{"Beef":"140"},"itemnumber2":{"Chicken":"200"}}', '340', ''),
+(7, 'Tuesday the 18th of November', '{"itemnumber1":{"Coffee":"324"}}', '324', ''),
+(8, 'Tuesday the 18th of November', '{"itemnumber1":{"Cheese":"783"}}', '783', ''),
+(9, 'Tuesday the 18th of November', '{"itemnumber1":{"Beer":"234"}}', '234', ''),
+(10, 'Tuesday the 18th of November', '{"itemnumber1":{"cheese":"687"},"itemnumber2":{"beef":"234"}}', '921', ''),
+(11, 'Wednesday the 19th of November', '{"itemnumber1":{"Bru":"187"}}', '187', ''),
+(12, 'Wednesday the 19th of November', '{"itemnumber1":{"Sand":"34"},"itemnumber2":{"Pigs":"674"}}', '708', ''),
+(13, 'Wednesday the 19th of November', '{"itemnumber1":{"Milk":"321"}}', '321', ''),
+(14, 'Thursday the 20th of November', '{"itemnumber1":{"Coffee":"234"}}', '234', ''),
+(15, 'Thursday the 20th of November', '{"itemnumber1":{"2 Mexican Veg Burgers":"456"}}', '456', ''),
+(16, 'Friday the 21st of November', '{"itemnumber1":{"food":"222"},"itemnumber2":{"cheese":"342"}}', '564', ''),
+(17, 'Monday the 24th of November', '{"itemnumber2":{"cake":"231"}}', '231', ''),
+(18, 'Thursday the 27th of November', '{"itemnumber1":{"big soup":"450"}}', '450', ''),
+(19, 'Friday the 5th of December 2014', '{"itemnumber1":{"crumpets":"378"},"itemnumber2":{"coffee":"230"}}', '608', ''),
+(20, 'Friday the 5th of December 2014', '{"itemnumber1":{"banana":"89"}}', '89', ''),
+(21, 'Friday the 5th of December 2014', '{"itemnumber1":{"More Coffee":"200"}}', '200', ''),
+(22, 'Friday the 5th of December 2014', '{"itemnumber1":{"Cheese Bread":"230"}}', '230', ''),
+(23, 'Friday the 5th of December 2014', '{"itemnumber1":{"Big Soup":"200"}}', '200', ''),
+(24, 'Friday the 5th of December 2014', '{"itemnumber1":{"2 Shortbread":"200"},"itemnumber2":{"Banana":"100"}}', '300', ''),
+(25, 'Monday the 8th of December 2014', '{"itemnumber1":{"Coffee":"200"}}', '200', ''),
+(26, 'Monday the 8th of December 2014', '{"itemnumber1":{"Feet":"89"}}', '89', 'Ashbury'),
+(27, 'Monday the 8th of December 2014', '{"itemnumber1":{"Chicken Nugger":"346"},"itemnumber2":{"Soy Sauwes":"85"}}', '431', 'intheon');
 
 -- --------------------------------------------------------
 
@@ -214,7 +218,7 @@ CREATE TABLE IF NOT EXISTS `exercise_tracker` (
   `exercises_done` varchar(255) NOT NULL,
   `gym_visits_left` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 --
 -- Dumping data for table `exercise_tracker`
@@ -222,7 +226,8 @@ CREATE TABLE IF NOT EXISTS `exercise_tracker` (
 
 INSERT INTO `exercise_tracker` (`id`, `user_id`, `date`, `exercises_done`, `gym_visits_left`) VALUES
 (15, 'intheon', 'Sunday the 7th of December', '', 3),
-(16, 'intheon', 'Friday the 5th of December 2014', '', 3);
+(16, 'intheon', 'Friday the 5th of December 2014', '', 3),
+(17, 'intheon', 'Monday the 8th of December 2014', '', 3);
 
 -- --------------------------------------------------------
 
@@ -236,14 +241,15 @@ CREATE TABLE IF NOT EXISTS `picture` (
   `picture_path` varchar(255) NOT NULL,
   `picture_taken` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `picture`
 --
 
 INSERT INTO `picture` (`id`, `week_number`, `picture_path`, `picture_taken`) VALUES
-(5, '1', 'C:/wamp/www/workout-diary/img/uploads/img_avocado1.png', 1);
+(5, '1', 'C:/wamp/www/workout-diary/img/uploads/img_avocado1.png', 1),
+(6, '2', 'C:/wamp/www/workout-diary/img/uploads/img_artisanal2.png', 1);
 
 -- --------------------------------------------------------
 
@@ -276,17 +282,18 @@ CREATE TABLE IF NOT EXISTS `timings` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `week_number` int(10) NOT NULL,
   `date` varchar(255) NOT NULL,
-  `day_of_week` int(10) NOT NULL,
   `days_in` int(10) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
 
 --
 -- Dumping data for table `timings`
 --
 
-INSERT INTO `timings` (`id`, `week_number`, `date`, `day_of_week`, `days_in`) VALUES
-(17, 1, 'Thursday the 4th of December 2014', 1, 338);
+INSERT INTO `timings` (`id`, `week_number`, `date`, `days_in`, `user_name`) VALUES
+(28, 1, 'Sunday the 23rd of November 2014', 327, 'intheon'),
+(30, 1, 'Monday the 8th of December 2014', 342, 'Ashbury');
 
 -- --------------------------------------------------------
 
