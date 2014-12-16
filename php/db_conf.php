ca16a9f563a526b3b1e145f15d83149f98be73f9
intheon <allobon@gmail.com>
2014-12-16 15:44:31 +0000

    production

--- php/db_conf.php
+++ php/db_conf.php
@@ -1,9 +1,9 @@
 <?php
 
-	$host = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "wholegrain";
+	$host = "5.102.184.24";
+	$username = "intheon_sir";
+	$password = "[E[tAkH_ye?f";
+	$database = "intheon_sonos";
 	$connect = mysqli_connect($host,$username,$password,$database);
 
 ?>
